1386423248ece2b9406f596454e489c45c4b3647
Prakhar Gupta <143396266+prax-1@users.noreply.github.com>
2024-01-27 12:12:31 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -8,9 +8,10 @@ vectorizer = joblib.load('tfidf_vectorizer.pkl')
 
 def classify_url():
     url = entry.get()
-    url_vectorized = vectorizer.transform([url])
 
+    url_vectorized = vectorizer.transform([url])
     prediction = rf_model.predict(url_vectorized)[0]
+
     result_label.config(text=f"Classification: {'Malicious' if prediction == 1 else 'Safe'}")
 
 
@@ -18,22 +19,28 @@ root = tk.Tk()
 root.title("Malicious URL Detector")
 root.geometry("400x200")
 
+root.configure(bg='#fff8e1')
+
 style = ttk.Style()
 style.configure("TButton", padding=(10, 5), font=('Helvetica', 12))
 
-header_label = ttk.Label(root, text="Malicious URL Detector", font=('Helvetica', 16, 'bold'))
-header_label.pack(pady=10)
+version_label = ttk.Label(root, text="Version 1.0", font=('Helvetica', 10, 'italic'), background='#fff8e1')
+version_label.pack(pady=5)
+
+header_label = ttk.Label(root, text="Malicious URL Detector", font=('Helvetica', 16, 'bold'), background='#fff8e1')
+header_label.pack(pady=5)
 
 entry = ttk.Entry(root, width=40, font=('Helvetica', 12))
-entry.pack(pady=10)
+entry.pack(pady=5)
 
 classify_button = ttk.Button(root, text="Classify URL", command=classify_url)
 classify_button.pack()
 
-result_label = ttk.Label(root, text="", font=('Helvetica', 14))
-result_label.pack(pady=10)
+result_label = ttk.Label(root, text="", font=('Helvetica', 14), background='#fff8e1')
+result_label.pack(pady=5)
 
-developed_by_label = ttk.Label(root, text="Developed By: Prakhar Gupta", font=('Helvetica', 10, 'italic'))
-developed_by_label.pack(side=tk.BOTTOM, pady=5)
+developed_by_label = ttk.Label(root, text="Developed By: Prakhar Gupta", font=('Helvetica', 10, 'italic'),
+                               background='#fff8e1')
+developed_by_label.pack()
 
 root.mainloop()
